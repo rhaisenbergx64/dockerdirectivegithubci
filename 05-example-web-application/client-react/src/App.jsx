36a3f77601160d1f68e6e6ad49e72e50030b16a8
rhaisenberg <toluwalashevincent@gmail.com>
2025-05-11 22:34:44 +0100

    commit continua2

--- 05-example-web-application/client-react/src/App.jsx
+++ 05-example-web-application/client-react/src/App.jsx
@@ -36,7 +36,7 @@ function CurrentTime(props) {
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>All thanks to Jah! 👋</h1>
+      <h1>All thanks to Jah, Amen ! 👋</h1>
       <CurrentTime api="/api/golang/"/>
       <CurrentTime api="/api/node/"/>
       <ReactQueryDevtools initialIsOpen={false} />
